630b06c0449688345d2b5ba05172d725a457db54
Connor <c.tebbatt@googlemail.com>
2026-03-24 13:47:02 +0000

    Refactor

--- cardsData.js
+++ cardsData.js
@@ -3,6 +3,7 @@ const cards = [
         id: 1,
         image: "rose.jpg",
         spelling: "Rosa",
+        category: "Red Plant",
         answer: {
             commonName: "Rose",
             latinName: "Rosa",
@@ -13,6 +14,7 @@ const cards = [
         id: 2,
         image: "lavender.jpg",
         spelling: "Lavandula angustifolia",
+        category: "Blue Plant",
         answer: {
             commonName: "Lavender",
             latinName: "Lavandula angustifolia",
@@ -23,6 +25,7 @@ const cards = [
         id: 3,
         image: "sunflower.jpeg",
         spelling: "Helianthus annuus",
+        category: "Yellow Plant",
         answer: {
             commonName: "Sunflower",
             latinName: "Helianthus annuus",
